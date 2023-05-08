6eb0f39b3722431093a18af8b59a284d88f227ab
Ode Ardika <dirfanardika@gmail.com>
2023-05-09 04:15:12 +0800

    Create function to add users to the database

--- Program/adminMenu.py
+++ Program/adminMenu.py
@@ -1,18 +1,39 @@
 from MYSQLConnector import connect
+import pyinputplus as pyip
+
 # Add Product to System
-def addProduct(nameProduct, priceProduct : int):
+def addProduct():
+    nameProduct = input("Name of Product: ")
+    priceProduct = pyip.inputInt("Price of Product: ")
     mydb = connect(addHost="localhost",addUser="root", addPassword="125125", addDB="store")
     mycursor =  mydb.cursor()
     sql = f"INSERT INTO item (nameItem, price) values (%s, %s)"
-    val = (f"{nameProduct}", priceProduct)
+    val = (nameProduct, priceProduct)
     mycursor.execute(sql, val)
     mydb.commit()
     print("Product Inserted")
     
 # Add Register User
 def registrationUser():
-    pass
-# Add Admin User
+    userName = input("User name: ")
+    userPass = 0
+    while userPass != confirmPass:
+        userPass = pyip.inputPassword("Input Password: ")
+        confirmPass = pyip.inputPassword("Confirm Password: ")
+        if userPass == "":
+            print("User registration is failed")
+            return 0
+        if userPass != confirmPass:
+            print("Password is not valid")
+    is_admin = pyip.inputBool("Is admin: ")
+    mydb = connect(addHost="localhost",addUser="root", addPassword="125125", addDB="store")
+    mycursor =  mydb.cursor()
+    sql = f"INSERT INTO users (fullname, pass, is_admin) values (%s, %s, %s)"
+    val = (userName , userPass, is_admin )
+    mycursor.execute(sql, val)
+    mydb.commit()
+    print("New user is inserted")    
+
 # Main Menu
 # Search by Product
 
